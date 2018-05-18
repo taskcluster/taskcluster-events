d3acd62688b9fc8c8a062b5d9ac0fc444c9707c6
ydidwania <didwaniayashvardhan+github@gmail.com>
2018-05-19 01:23:16 +0530

    started on  main.js

---
+++ src/main.js
@@ -0,0 +1,54 @@
+#!usr/bin/env node
+let loader = require('taskcluster-lib-loader');
+let App = require('taskcluster-lib-app');
+let monitor = require('taskcluster-lib-monitor');
+let debug = require('debug')('app:main');
+let config = require('typed-env-config');
+
+
+// Create component loader
+let load = loader({
+  cfg: {
+    requires: ['profile'],
+    setup: ({profile}) => config({profile}),
+  },
+
+  monitor: {
+    requires: ['process', 'profile', 'cfg'],
+    setup: ({process, profile, cfg}) => monitor({
+      project: 'taskcluster-queue',
+      credentials: cfg.taskcluster.credentials,
+      mock: cfg.monitor.mock,
+      process,
+    }),
+  },
+
+  server: {
+    requires: ['cfg', 'monitor'],
+    setup: ({cfg, monitor, docs}) => {
+      let app = App({
+        port: cfg.server.port,
+        publicUrl: cfg.server.publicUrl,
+        env: cfg.server.env,
+        forceSSL: cfg.server.forceSSL,
+        trustProxy: cfg.server.trustProxy,
+        rootDocsLink: true,
+      });
+      return app.createServer();
+    },
+  },
+}, ['profile','process']);
+
+// If this file is executed launch component from first argument
+if (!module.parent) {
+  load(process.argv[2], {
+    process: process.argv[2],
+    profile: process.env.NODE_ENV,
+  }).catch(err => {
+    console.log(err.stack);
+    process.exit(1);
+  });
+}
+
+// Export load for tests
+module.exports = load;
